9bc74499180c34a2373e1a8979db23e1538ea446
The-sad-philosopher <shinudonney@tuta.io>
2020-12-22 09:43:09 +0530

    Control field and store user input in state 🌈

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import './SearchBar.sass';
 
 export class SearchBar extends Component {
+  state = {
+    userInput: '',
+  };
+
+  onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      userInput: event.target.value,
+    });
+  };
+
+  onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(this.state.userInput);
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="image-search">Image Search</label>
-            <input type="text" placeholder="Search" />
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={this.onInputChange}
+              value={this.state.userInput}
+            />
             <i className="search icon"></i>
           </div>
         </form>
